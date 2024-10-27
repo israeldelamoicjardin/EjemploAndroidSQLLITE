1137c2aaa11953278c8926d076b701768a134f00
titan\israel <blas@irakasle.eus>
2024-10-27 14:53:45 +0100

    ADD Actividad para añadir notas creada

---
+++ app/src/main/java/es/israeldelamo/ejemplosqliteandroid/AgregarNotaMainActivity.kt
@@ -0,0 +1,15 @@
+package es.israeldelamo.ejemplosqliteandroid
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AgregarNotaMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_nota_main)
+
+    }
+}
