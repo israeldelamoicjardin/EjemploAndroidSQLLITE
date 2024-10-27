751b6298f75b4cd345a4a525524c8bec00bac7ab
titan\israel <blas@irakasle.eus>
2024-10-27 17:06:17 +0100

    FASE Añade datos a la base de datos con éxito

--- app/src/main/java/es/israeldelamo/ejemplosqliteandroid/AgregarNotaMainActivity.kt
+++ app/src/main/java/es/israeldelamo/ejemplosqliteandroid/AgregarNotaMainActivity.kt
@@ -1,15 +1,60 @@
 package es.israeldelamo.ejemplosqliteandroid
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import es.israeldelamo.ejemplosqliteandroid.databinding.ActivityAgregarNotaMainBinding
+import es.israeldelamo.ejemplosqliteandroid.databinding.ActivityMainBinding
+
+/**
+ * En esta clase llamamos al gestor de base de datos
+ * leemos los campos
+ * insertamos y modificamos a placer
+ */
 
 class AgregarNotaMainActivity : AppCompatActivity() {
+
+    //como siempre usamos el binding, debe coincidir con la declaración de activity que hayamos hecho
+    /**
+     * binding para los datos con lo grafico
+     */
+    private lateinit var binding: ActivityAgregarNotaMainBinding
+
+    /**
+     * conexion a la base de datos
+     */
+    private lateinit var db : NotaDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        //nuestras tres lineas favoritas para hacer binding
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_agregar_nota_main)
+        binding = ActivityAgregarNotaMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //comenzamos con la db
+        db = NotaDatabaseHelper(this )
+
+        //enlazamos el boton de chekc con la base de datos
+        binding.ivGuardarNota.setOnClickListener {
+            val titulo = binding.etTitulo.text.toString()
+            val descripcion = binding.etDescripcion.text.toString()
+            //crepo el objeto nota
+            val nota = Nota(0,titulo,descripcion)
+            //lo inserto
+            db.insertNota(nota)
+            //cierro esta activity y abro la main
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            //limpiamos la pila
+            finishAffinity()
+            Toast.makeText(applicationContext, "Se ha agreagado la nota", Toast.LENGTH_LONG).show()
+
+
+        }
 
     }
 }
--- app/src/main/java/es/israeldelamo/ejemplosqliteandroid/NotaDatabaseHelper.kt
+++ app/src/main/java/es/israeldelamo/ejemplosqliteandroid/NotaDatabaseHelper.kt
@@ -1,5 +1,6 @@
 package es.israeldelamo.ejemplosqliteandroid
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -17,7 +18,7 @@ class NotaDatabaseHelper (context: Context) : SQLiteOpenHelper(
         private const val   TABLE_NAME = "notas"
         // y ahora las columnas o atributos
         private const val   COLUMN_ID = "id"
-        private const val   COLUMN_TITLE = "id"
+        private const val   COLUMN_TITLE = "titulo"
         private const val   COLUMN_DESCRIPCION = "descripcion"
 
     }
@@ -34,8 +35,35 @@ class NotaDatabaseHelper (context: Context) : SQLiteOpenHelper(
         db?.execSQL(createTableQuery)
     }
 
+    /**
+     * Modifica la tabla de la base de datos
+     */
+
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+        val dropTableQuery =
+            "DROP TABLE IF EXISTS $TABLE_NAME"
+        db?.execSQL(dropTableQuery)
+        // ya hemos mirado si existia o no, ahora la podemos crear sin que de errores
+        onCreate(db)
+    }
+
+    /**
+     * Pasado un objeto nota, la añade a la base de datos
+     * @param nota la nota a añadir
+     */
+
+    fun insertNota(nota: Nota){
+        //la abro en modo escritura
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_TITLE,       nota.titulo)
+            put(COLUMN_DESCRIPCION, nota.descripcion)
+            //en este caso ID es autonumérico
+        }
+        //insertamos datos
+        db.insert(TABLE_NAME, null, values)
+        //y ahora a cerrar
+        db.close()
     }
 
 }
