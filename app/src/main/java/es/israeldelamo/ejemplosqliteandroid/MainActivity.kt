0ce92ed3ad682384a0666a4235cae366f7defea8
titan\israel <blas@irakasle.eus>
2024-10-27 14:19:35 +0100

    ADD toast creado

--- app/src/main/java/es/israeldelamo/ejemplosqliteandroid/MainActivity.kt
+++ app/src/main/java/es/israeldelamo/ejemplosqliteandroid/MainActivity.kt
@@ -1,20 +1,32 @@
 package es.israeldelamo.ejemplosqliteandroid
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import es.israeldelamo.ejemplosqliteandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * Binding de la escena con los elementos visuales
+     */
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Con tres lineas asocio mediante el binding lo grafico y su programacion
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //ahora podemos hacer referncia al botón mediante el binding y monstar un mensajin
+        binding.FABAgregarNota.setOnClickListener{
+            Toast.makeText(applicationContext,
+                "Botón pulsado",
+                Toast.LENGTH_SHORT).show()
         }
     }
 }
